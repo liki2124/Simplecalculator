e2f4e281a4e1164252a10ec23bb1ceac466a8a53
Likitha S (IN62391) <M1062391@mindtree.com>
2021-02-23 15:13:22 +0530

    https://github.com/liki2124/Simplecalculator.git

---
+++ src/main/java/com/mindtree/simplecal/simplecal.java
@@ -0,0 +1,17 @@
+package com.mindtree.simplecal;
+
+public class simplecal {
+
+	public static void main(String args[])
+	{
+	
+	int a=10,b=20;
+	int sum;
+	int difference;
+	sum = a+b;
+	difference = a-b;
+	System.out.println("the sum is "+sum);
+	System.out.println("the difference is:"+difference);
+}
+}
+
